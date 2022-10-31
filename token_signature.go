19c5864cea924e35d0a982d6eb33231144f6d159
Andrea Vasapollo <andrea.vasapollo@checkout.com>
2022-10-31 10:52:26 +0000

    chore: removing C

--- token_signature.go
+++ token_signature.go
@@ -1,16 +1,13 @@
 package applepay
 
-import "C"
 import (
 	"bytes"
 	"crypto/x509"
-	"encoding/binary"
 	"encoding/hex"
 	"encoding/pem"
 	"fmt"
 	"io/ioutil"
 	"time"
-	"unsafe"
 
 	"github.com/pkg/errors"
 	"go.mozilla.org/pkcs7"
@@ -29,32 +26,38 @@ func (t *PKPaymentToken) verifySignature() error {
 		return errors.Wrap(err, "invalid version")
 	}
 
-	//TODO: parse p7
+	// parse p7
 	p7, err := pkcs7.Parse(t.PaymentData.Signature)
 	if err != nil {
 		return fmt.Errorf("cannot parse our signed data: %s", err.Error())
 	}
-
-	// Load
-	inter := p7.Certificates[1]
-	leaf := p7.Certificates[0]
 
 	root, err := loadRootCertificate(AppleRootCertificatePath)
 	if err != nil {
 		return errors.Wrap(err, "error loading the root certificate")
 	}
 
+	if len(p7.Certificates) < 2 {
+		return errors.New("the len of certificates is less than 2")
+	}
+
+	// Load
+	inter := p7.Certificates[1]
+	leaf := p7.Certificates[0]
+
 	// Verify
 	if err := verifyCertificates(root, inter, leaf); err != nil {
 		return errors.Wrap(err, "error when verifying the certificates")
 	}
 
-	// TODO: verify PKCS7 signature
+	// create the cert poll with iter and leaf
 	pool := x509.NewCertPool()
 	pool.AddCert(root)
 	pool.AddCert(inter)
 	pool.AddCert(leaf)
 
+	// verifyPKCS7Signature verifies that the signature was produced by the leaf
+	// certificate contained in the given PKCS7 struct
 	if err := p7.VerifyWithChain(pool); err != nil {
 		return err
 	}
@@ -67,80 +70,6 @@ func (t *PKPaymentToken) verifySignature() error {
 	}
 
 	return nil
-}
-
-// decodePKCS7 decodes the raw payment token signature field into an OpenSSL
-// PKCS7 struct, and returns the intermediary and leaf certificates used for the
-// signature
-func (t PKPaymentToken) decodePKCS7() (p7 *C.PKCS7, inter,
-	leaf *x509.Certificate, err error) {
-
-	// Decode PKCS7 blob, certificate chain
-	pkcs7Bio := newBIOBytes(t.PaymentData.Signature)
-	defer pkcs7Bio.Free()
-	p7 = C.d2i_PKCS7_bio(pkcs7Bio.C(), nil)
-	if p7 == nil {
-		err = errors.New("openssl error: could not decode PKCS7")
-		return
-	}
-	sign := (*C.PKCS7_SIGNED)(union(p7.d))
-
-	if sign == nil {
-		C.PKCS7_free(p7)
-		err = errors.New("openssl error: error dereferencing d in PKCS7")
-		return
-	}
-
-	// Decode intermediate and leaf certificates
-	if inter, leaf, err = decodeIntermediateAndLeafCert(sign); err != nil {
-		C.PKCS7_free(p7)
-		err = errors.Wrap(err, "error decoding the embedded certificates")
-		return
-	}
-
-	return p7, inter, leaf, nil
-}
-
-// decodeIntermediateAndLeafCert decodes the intermediary and leaf certificates
-// of the PKCS7 object
-func decodeIntermediateAndLeafCert(sign *C.PKCS7_SIGNED) (inter,
-	leaf *x509.Certificate, err error) {
-
-	const interCertIndex = 1
-	const leafCertIndex = 0
-
-	// C structs -> DER
-	// TODO: check stack length
-	interBio, leafBio := newBIO(), newBIO()
-	defer interBio.Free()
-	defer leafBio.Free()
-	r := C.i2d_X509_bio(
-		interBio.C(),
-		C.sk_X509_value_func(sign.cert, interCertIndex),
-	)
-	if r != 1 {
-		err = errors.Wrap(opensslErr(), "error encoding the intermediate cert")
-		return
-	}
-	r = C.i2d_X509_bio(
-		leafBio.C(),
-		C.sk_X509_value_func(sign.cert, leafCertIndex),
-	)
-	if r != 1 {
-		err = errors.Wrap(opensslErr(), "error encoding the leaf cert")
-		return
-	}
-
-	// DER -> Go structs
-	if inter, err = x509.ParseCertificate(interBio.ReadAll()); err != nil {
-		err = errors.Wrap(err, "error decoding the intermediate certificate")
-		return
-	}
-	if leaf, err = x509.ParseCertificate(leafBio.ReadAll()); err != nil {
-		err = errors.Wrap(err, "error decoding the leaf certificate")
-		return
-	}
-	return
 }
 
 // loadRootCertificate loads the root certificate from the disk
@@ -187,29 +116,6 @@ func verifyCertificates(root, inter, leaf *x509.Certificate) error {
 		return errors.Wrap(err, "leaf cert is not trusted by intermediate cert")
 	}
 
-	return nil
-}
-
-// verifyPKCS7Signature verifies that the signature was produced by the leaf
-// certificate contained in the given PKCS7 struct
-func (t PKPaymentToken) verifyPKCS7Signature(p7 *pkcs7.PKCS7) error {
-	// TODO: use the Go x509 API instead of OpenSSL
-	// This code does not work for some reason:
-	// if err := leaf.CheckSignature(leaf.SignatureAlgorithm, t.signedData(),
-	//     signatureBytes); err != nil {
-	//
-	//     return errors.Wrap(err, "invalid signature")
-	// }
-
-	p7.Verify()
-	signedDataBio := newBIOBytes(t.signedData())
-	defer signedDataBio.Free()
-	// The PKCS7_NOVERIFY flag corresponds to verifying the chain of trust of
-	// the certificates, which should have been done before
-	r := C.PKCS7_verify(p7, nil, nil, signedDataBio.C(), nil, C.PKCS7_NOVERIFY)
-	if r != 1 {
-		return errors.Wrap(opensslErr(), "signature validation error")
-	}
 	return nil
 }
 
@@ -262,65 +168,4 @@ func (t PKPaymentToken) verifySigningTime(p7 *pkcs7.PKCS7) error {
 		)
 	}
 	return nil
-}
-
-// extractSigningTime returns the time of signing from a PKCS7 struct
-func (t PKPaymentToken) extractSigningTime(p7 *pkcs7.PKCS7) (time.Time, error) {
-	signerInfoList := C.PKCS7_get_signer_info(p7)
-	if signerInfoList == nil {
-		return time.Time{},
-			errors.New("openssl error when extracting signer information")
-	}
-
-	// Find the right SIGNER_INFO field
-	signerInfoListSize := int(C.sk_PKCS7_SIGNER_INFO_num_func(signerInfoList))
-	var signingTime time.Time
-	for i := 0; i < signerInfoListSize; i++ {
-		si := C.sk_PKCS7_SIGNER_INFO_value_func(signerInfoList, C.int(i))
-		if si == nil {
-			continue
-		}
-		so := C.PKCS7_get_signed_attribute(si, C.NID_pkcs9_signingTime)
-		if so == nil || so._type != C.V_ASN1_UTCTIME {
-			continue
-		}
-
-		// Decode the signing time
-		stBio := newBIO()
-		r := C.ASN1_UTCTIME_print(stBio.C(), (*C.ASN1_UTCTIME)(union(so.value)))
-		if r != 1 {
-			stBio.Free()
-			return time.Time{}, errors.Wrap(opensslErr(), "time encoding error")
-		}
-		pt, err := time.Parse("Jan _2 15:04:05 2006 MST", stBio.ReadAllString())
-		if err != nil {
-			stBio.Free()
-			return time.Time{}, errors.Wrap(err, "error parsing time")
-		}
-		signingTime = pt
-		stBio.Free()
-		break
-	}
-	if signingTime.IsZero() {
-		return time.Time{}, errors.New("signing time not found")
-	}
-
-	return signingTime, nil
-}
-
-// union dereferences a union pointer so that its value can be used
-// Don't do this at home!
-func union(union [8]byte) unsafe.Pointer {
-	dBuf := bytes.NewBuffer(union[:])
-	var ptr uint64
-	binary.Read(dBuf, binary.LittleEndian, &ptr)
-	return unsafe.Pointer(uintptr(ptr))
-}
-
-// opensslErr reads the errors from OpenSSL into a Go error
-func opensslErr() error {
-	errOut := newBIO()
-	defer errOut.Free()
-	C.ERR_print_errors(errOut.C())
-	return errors.New(errOut.ReadAllString())
 }
